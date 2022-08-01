2f8e7b1538e452d36bc2730a95da1c819856f4cb
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-01 20:14:02 -0300

    LOGIC => REGISTER USER

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose
 
 app.use(express.json());
 
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
