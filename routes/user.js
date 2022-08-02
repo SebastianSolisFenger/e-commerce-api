216274796901fe22f6e03ba6b20b387dbb335b17
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-02 16:53:34 -0300

    // GET ALL USERS

--- routes/user.js
+++ routes/user.js
@@ -3,31 +3,11 @@ const User = require('../models/user');
 const {
   verifyToken,
   verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
 } = require('../routes/verifyToken');
 
-// UPDATE
-// router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
-//   if (req.body.password) {
-//     req.body.password = CryptoJS.AES.encrypt(
-//       req.body.password,
-//       // @ts-ignore
-//       process.env.PASS_SEC
-//     ).toString();
-//   }
-//   try {
-//     const updatedUser = await User.findByIdAndUpdate(
-//       req.params.id,
-//       {
-//         $set: req.body,
-//       },
-//       { new: true }
-//     );
-//     res.status(200).json(updatedUser);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 //UPDATE
+
 router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
@@ -45,6 +25,40 @@ router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
       { new: true }
     );
     res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('User has been deleted');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET USER
+router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET ALL USERS
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(1)
+      : await User.find();
+    res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
   }
