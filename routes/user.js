bb30df6478af0f383e40ea6780d025362fb83e02
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-02 18:15:13 -0300

    FINISHED USER  BASE

--- routes/user.js
+++ routes/user.js
@@ -56,9 +56,40 @@ router.get('/', verifyTokenAndAdmin, async (req, res) => {
   const query = req.query.new;
   try {
     const users = query
-      ? await User.find().sort({ _id: -1 }).limit(1)
+      ? await User.find().sort({ _id: -1 }).limit(5)
       : await User.find();
     res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET USER STATS WITH MONGODB
+router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  // return the last year
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+  try {
+    const data = await User.aggregate([
+      {
+        // less than today and greater than last year
+        $match: { createdAt: { $gte: lastYear } },
+      },
+      {
+        $project: {
+          month: {
+            $month: '$createdAt',
+          },
+        },
+      },
+      {
+        $group: {
+          _id: '$month',
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
   } catch (err) {
     res.status(500).json(err);
   }
