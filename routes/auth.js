1c4c5508eb9c12e27f03e24c69350d83c3a3951d
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-02 12:35:30 -0300

    Code logic => LOGIN

--- routes/auth.js
+++ routes/auth.js
@@ -21,4 +21,32 @@ router.post('/register', async (req, res) => {
   }
 });
 
+// LOGIN USER
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
+
+    // IF THREE IS NO USER
+    !user && res.status(401).json('Wrong credentials!');
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    // WRONG PASSWORD
+    password !== req.body.password &&
+      res.status(401).json('Wrong credentials!');
+
+    // IF ALL IS OK
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
