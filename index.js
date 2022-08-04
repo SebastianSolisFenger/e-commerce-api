600fc99c3e2a942d3960dde37462059441ca6b27
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-04 11:33:44 -0300

    LATEST

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
 const cartRoute = require('./routes/cart');
 const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -56,6 +58,8 @@ mongoose
 //   console.log('test is successful');
 // });
 
+app.use(cors());
+
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
@@ -63,6 +67,7 @@ app.use('/api/users', userRoute);
 app.use('/api/products', productRoute);
 app.use('/api/carts', cartRoute);
 app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('BackEnd Server is running!');
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+// const stripe = require('stripe')(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY;
+const stripe = require('stripe')(KEY);
 
 router.post('/payment', async (req, res) => {
   stripe.charges.create(
@@ -19,3 +21,20 @@ router.post('/payment', async (req, res) => {
 });
 
 module.exports = router;
+// router.post("/payment", (req, res) => {
+//   stripe.charges.create(
+//     {
+//       source: req.body.tokenId,
+//       amount: req.body.amount,
+//       currency: "usd",
+//     },
+//     (stripeErr, stripeRes) => {
+//       if (stripeErr) {
+//         res.status(500).json(stripeErr);
+//       } else {
+//         res.status(200).json(stripeRes);
+//       }
+//     }
+//   );
+// });
+// module.exports = router;
